99321855aec897facd7865e58728325332509571
dx.wang <wdongxv@gmail.com>
2016-03-24 12:53:48 +0800

    fix remoteConnectMaxTime

--- DDDProxy/localToRemoteConnectManger.py
+++ DDDProxy/localToRemoteConnectManger.py
@@ -36,26 +36,26 @@ class remoteServerConnecter(symmetryConnectServerHandler):
 					self.server.addCallback(connect.setServerAuthPass)
 				self.authCallbackList = []
 				self.authPass = True
-				self.connectName = "[remote:"+str(self.fileno())+"]	"+self.address[0]
+				self.connectName = "[remote:" + str(self.fileno()) + "]	" + self.address[0]
 				
 			else:
 				self.close()
-	def addLocalRealConnect(self,connect):
+	def addLocalRealConnect(self, connect):
 		self.addSymmetryConnect(connect, self.makeSymmetryConnectId())
 		if self.authPass:
 			self.server.addCallback(connect.setServerAuthPass)
-	def auth(self,auth):
+	def auth(self, auth):
 		timenum = math.floor(time.time())
 		data = {"opt":"auth"}
 		data.update(self.authMake(auth, timenum))
-		self.sendData(symmetryConnectServerHandler.serverToServerJsonMessageConnectId,json.dumps(data))
+		self.sendData(symmetryConnectServerHandler.serverToServerJsonMessageConnectId, json.dumps(data))
 	def onClose(self):
 		symmetryConnectServerHandler.onClose(self)
 
 class localToRemoteConnectManger():
 	"""
 	"""
-	def __init__(self,server):
+	def __init__(self, server):
 		self.server = server;
 		self.remoteConnectList = {}
 		self.remoteConnectListLoop = 0
@@ -67,36 +67,36 @@ class localToRemoteConnectManger():
 		remoteServerList = settingConfig.setting(settingConfig.remoteServerList)
 		if remoteServerList == None:
 			return None
-		if self.remoteConnectListLoop >= len(remoteServerList)*maxConnectByOnServer:
+		if self.remoteConnectListLoop >= len(remoteServerList) * maxConnectByOnServer:
 			self.remoteConnectListLoop = 0
 		i = 0
 		for remoteServer in remoteServerList:
 			connectList = None
 			port = int(remoteServer["port"]) if remoteServer["port"] else 8082
-			remoteServerKey = remoteServer["host"]+":"+str(port)
+			remoteServerKey = remoteServer["host"] + ":" + str(port)
 			if remoteServerKey in self.remoteConnectList:
 				connectList = self.remoteConnectList[remoteServerKey]
 			else:
 				self.remoteConnectList[remoteServerKey] = connectList = {}
 				
 			if  self.remoteConnectListLoop >= i and self.remoteConnectListLoop < i + maxConnectByOnServer:
-				index = self.remoteConnectListLoop-i
+				index = self.remoteConnectListLoop - i
 				connect = None
 				if index in connectList:
 					connect = connectList[index]
-					if not connect.connectStatus() or ( remoteConnectMaxTime > 600 and connect.info["startTime"] + remoteConnectMaxTime < time.time()):
+					if (not connect.connectStatus()) or (connect.info["startTime"] + max(remoteConnectMaxTime, 600) < time.time()):
 						connect = None
 				if not connect:
 					connect = remoteServerConnecter(self.server)
-					connect.connect((remoteServer["host"],port),True)
+					connect.connect((remoteServer["host"], port), True)
 					connect.auth(remoteServer["auth"])
 					connectList[index] = connect
 				return connect
 			i += maxConnectByOnServer
 		return None
-	def onConnectClose(self,connect):
-		for _,connectList in self.remoteConnectList.items():
-			for k,v in connectList.items():
+	def onConnectClose(self, connect):
+		for _, connectList in self.remoteConnectList.items():
+			for k, v in connectList.items():
 				if v == connect:
 					del connectList[k]
 					return
@@ -107,9 +107,9 @@ class localToRemoteConnectManger():
 		if not localToRemoteConnectManger.manager:
 			localToRemoteConnectManger.manager = localToRemoteConnectManger(server)
 	@staticmethod
-	def getConnectHost(host,port):
+	def getConnectHost(host, port):
 		port = port if str(port) else "8082"
-		return localToRemoteConnectManger.manager.remoteConnectList[host+":"+port]
+		return localToRemoteConnectManger.manager.remoteConnectList[host + ":" + port]
 		
 	@staticmethod
 	def getConnect():
